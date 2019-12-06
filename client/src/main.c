3e6f344ab7c2edbb10dcb4128c8a0483e618a319
henri <pnpeepdc@gmail.com>
2019-12-06 09:37:55 +0100

    ajout commande sim dans le client

--- client/src/main.c
+++ client/src/main.c
@@ -232,8 +232,9 @@ int main(int argc, char** argv)
 
         } else if (strncmp(sent, "END", len) == 0) {
             end = 1;
-        } else if (strncmp(sent, "\033A", len) == 0) {
 
+        }
+        else if (strncmp(sent, "\033A", len) == 0) {
             puts("CANN CHECK_ENGINE x avec x = 0 eteint et 1 allumé");
         } else {
             if (validate_message(arg.message) == -1) {
