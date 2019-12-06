40afa2ad5e371680a35b7e64521334f1cf58227a
henri <pnpeepdc@gmail.com>
2019-12-06 15:52:29 +0100

    modif pour maroua

--- serveur/Maroua/maroua_scene.cpp
+++ serveur/Maroua/maroua_scene.cpp
@@ -1,4 +1,5 @@
 #include "maroua_scene.h"
+#include "Henri/onoff.h"
 
 #include <QGraphicsView>
 #include<QDateTime>
@@ -7,18 +8,22 @@ maroua_scene::maroua_scene(scene_globale *parent) :
     scene_globale(parent)
 {
 
-    cadrantessence *essence= new cadrantessence();
-    cadranttour *tour = new cadranttour();
+//    cadrantessence *essence= new cadrantessence();
+//    cadranttour *tour = new cadranttour();
     Vitesse = new cadrantvitesse();
     CompteTours = new cadranttour();
     //cadranttempmoteur *temp=new cadranttempmoteur();
-    cadrantcontour *contour=new cadrantcontour();
+//    cadrantcontour *contour=new cadrantcontour();
+    CompteurKm=new onoff();
 
 
-    addItem(contour);
+//    addItem(contour);
     addItem(Vitesse);
-    addItem(essence);
-    addItem(tour);
+//    addItem(essence);
+//    addItem(tour);
+
+    addItem(CompteTours);
+    addItem(CompteurKm);
 
 
 
