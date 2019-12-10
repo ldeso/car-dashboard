26e96472812d8771488f58ed527e96a2cb9274f3
Inna Lobanova <inna_lobanova@bk.ru>
2019-12-10 09:38:59 +0100

    fixed warnings

--- serveur/mainwindow.h
+++ serveur/mainwindow.h
@@ -27,7 +27,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     void acceleration(int);
     void simulation(int);
     ~MainWindow();
